9d6933c3106cf1cc607421bbbe1c821095232557
Samuele Mazzei <samumaz01@gmail.com>
2022-10-05 23:02:31 +0200

    prima versione funzionante

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const puppeteer = require("puppeteer");
 const fs = require("fs");
+const TelegramBot = require("node-telegram-bot-api");
+require("dotenv").config();
+
+const token = process.env.TELEGRAM_TOKEN;
+let bot = new TelegramBot(token, { polling: true });
 
 (async () => {
   const LAST_EVENT_SELECTOR = "ul.event-wrapper ";
   const EVENT_SELECTOR = LAST_EVENT_SELECTOR;
   const DOCUMENT_SELECTOR = ".document-row";
-  const documentsArray = [];
-  let oldDocumentsArray = [];
-
-  if (fs.existsSync("documents.json")) {
-    oldDocumentsArray = JSON.parse(fs.readFileSync("documents.json", "utf8"));
-  }
 
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
@@ -20,54 +19,68 @@ const fs = require("fs");
       waitUntil: "networkidle2",
     }
   );
+  while (true) {
+    console.log("Checking for new documents...");
+    const documentsArray = [];
+    let oldDocumentsArray = [];
 
-  //get reference of all nodes with selector ".document-row"
-  const documentNodes = await page.$$(EVENT_SELECTOR + DOCUMENT_SELECTOR);
-  console.log("Found " + documentNodes.length + " documents");
+    if (fs.existsSync("documents.json")) {
+      oldDocumentsArray = JSON.parse(fs.readFileSync("documents.json", "utf8"));
+    }
 
-  //loop through the nodes and get the innerText and href
-  for (let i = 0; i < documentNodes.length; i++) {
-    const documentNode = documentNodes[i];
-    const documentNameTmp = await page.evaluate(
-      (node) => node.innerText,
-      documentNode
-    );
+    //get reference of all nodes with selector ".document-row"
+    const documentNodes = await page.$$(EVENT_SELECTOR + DOCUMENT_SELECTOR);
+    console.log("Found " + documentNodes.length + " documents");
 
-    //clean innerText
-    const documentName = documentNameTmp.replace(/\s+/g, " ").trim();
+    //loop through the nodes and get the innerText and href
+    for (let i = 0; i < documentNodes.length; i++) {
+      const documentNode = documentNodes[i];
+      const documentNameTmp = await page.evaluate(
+        (node) => node.innerText,
+        documentNode
+      );
 
-    //get document  download link
-    const documentLink = await page.evaluate(
-      (node) => node.firstChild.href,
-      documentNode
-    );
+      //clean innerText
+      const documentName = documentNameTmp.replace(/\s+/g, " ").trim();
 
-    const document = {
-      name: documentName,
-      // date: documentDate,
-      link: documentLink,
-    };
+      //get document  download link
+      const documentLink = await page.evaluate(
+        (node) => node.firstChild.href,
+        documentNode
+      );
 
-    documentsArray.push(document);
+      const document = {
+        name: documentName,
+        // date: documentDate,
+        link: documentLink,
+      };
+
+      documentsArray.push(document);
+    }
+
+    //compare old and new array
+    const newDocumentsArray = documentsArray.filter((document) => {
+      return !oldDocumentsArray.some((oldDocument) => {
+        return oldDocument.name === document.name;
+      });
+    });
+    console.log("Found " + newDocumentsArray.length + " new documents");
+
+    //for each new document send a message to telegram
+    newDocumentsArray.forEach((document) => {
+      bot.sendMessage(
+        process.env.TELEGRAM_CHAT_ID,
+        document.name + " " + document.link
+      );
+    });
+
+    // save the new documents to a file
+    fs.writeFileSync("documents.json", JSON.stringify(documentsArray));
+    
+    //wait for 10 seconds
+    await page.waitForTimeout(10000);
   }
 
-  //compare old and new array
-  const newDocumentsArray = documentsArray.filter((document) => {
-    return !oldDocumentsArray.some((oldDocument) => {
-      return oldDocument.name === document.name;
-    });
-  });
-  console.log("Found " + newDocumentsArray.length + " new documents");
-
-  // save the new documents to a file
-  fs.writeFileSync("documents.json", JSON.stringify(documentsArray));
-  console.log("Done!");
-
-  // const read = fs.readFileSync("documents.json", { encoding: "utf8" });
-  // console.log(JSON.parse(read)[0]);
-
   //wait for 0 minutes then close browser
-  setTimeout(async () => {
-    await browser.close();
-  }, 0);
+  await browser.close();
 })();
